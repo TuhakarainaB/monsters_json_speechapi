1c497fa0bfeb3119b5d933bbfa22f206f7363699
TuhakarainaB <Bruce.Tuhakaraina@toiohomai.ac.nz>
2023-09-07 09:24:32 +1200

    Added Nav menu that when user clicks on link, this will open corresponding object from JSON file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // reference to our root div
 const rootDiv = document.getElementById('root');
+const monsterNav = document.getElementById('monsterNav');
 
 // use fetch to load monsters.json file
 // remember fetch has 2 promises (.then) and a catch.
@@ -10,33 +11,49 @@ fetch('monsters.json')
     monsters => {
         monsters.forEach(
             monster => {
-                const name = document.createElement('h1');
-                name.textContent = monster.monster;
-                const location = document.createElement('p');
-                location.innerHTML = `<strong>Location: </strong>${monster.location}`;
-                const movies = document.createElement('p');
-                movies.innerHTML = `<strong>Movies: </strong>${monster.Movies}`;
 
-                rootDiv.appendChild(name);
-                rootDiv.appendChild(location);
-                rootDiv.appendChild(movies);
+                const monsterLink = document.createElement('a');
+                monsterLink.href = "#";
+                monsterLink.textContent = monster.monster;
 
-                // Create a button for each JSON object
-                const button = document.createElement('button');
-                button.textContent = "Read Monster Name";
+                monsterLink.addEventListener(
+                    'click', (e) => {
+                        e.preventDefault(); // prevent browser default handling of links
 
-                // Add an event listener to the button to read the desired content
-                button.addEventListener('click',
-                () => {
-                    // tell browser content to read or speak
-                    const read = new SpeechSynthesisUtterance(monster.monster);
-                    // do the actual speaking...
-                    window.speechSynthesis.speak(read);
-                }
+                        // clear the rootdiv before displaying new monster details
+                        rootDiv.innerHTML = "";
+
+                        const name = document.createElement('h1');
+                        name.textContent = monster.monster;
+                        const location = document.createElement('p');
+                        location.innerHTML = `<strong>Location: </strong>${monster.location}`;
+                        const movies = document.createElement('p');
+                        movies.innerHTML = `<strong>Movies: </strong>${monster.Movies}`;
+
+                        rootDiv.appendChild(name);
+                        rootDiv.appendChild(location);
+                        rootDiv.appendChild(movies);
+
+                        // Create a button for each JSON object
+                        const button = document.createElement('button');
+                        button.textContent = "Read Monster Name";
+
+                        // Add an event listener to the button to read the desired content
+                        button.addEventListener('click',
+                        () => {
+                            // tell browser content to read or speak
+                            const read = new SpeechSynthesisUtterance(monster.monster);
+                            // do the actual speaking...
+                            window.speechSynthesis.speak(read);
+                        }
+                        );
+
+                        // append button to root div
+                        rootDiv.appendChild(button);
+                    }
                 );
 
-                // append button to root div
-                rootDiv.appendChild(button);
+                monsterNav.appendChild(monsterLink);
             }
         );
     }
